68ede3447d00ea48f0a1ae89e1d20961a682d545
Ivan Leon <ivanleonaguilar@hotmail.com>
2020-11-02 18:02:18 -0800

    Added sleep function to utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,9 @@
 export function compare(a, b, c) {
   return (a == b) && (b == c);
 }
+
+export function sleep(ms) {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, ms);
+  });
+}
